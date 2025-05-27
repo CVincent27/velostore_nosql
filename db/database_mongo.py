fa25271fa1865bb40bc777701828e2b9080b12fd
VincentC27 <18120867+CVincent27@users.noreply.github.com>
2025-05-27 18:02:20 +0200

    feat: add test db mongp

--- db/database_mongo.py
+++ db/database_mongo.py
@@ -1,4 +1,6 @@
+import pymongo
 from pymongo import MongoClient
+
 
 class VelostoreDatabaseMongo():
     """Classe pour gérer la base de données Velostore."""
@@ -6,20 +8,10 @@ class VelostoreDatabaseMongo():
     def __init__(self):
         """Initialise la connexion à la base de données."""
         self.connection_db()
-        self.create_schema_bike_item()
-        self.create_collection()
 
     def connection_db(self) -> None:
-        """Crée une connexion et un objet curseur pour la base de données."""
+        """Crée connexion pour la base de données."""
         client = MongoClient("mongodb://localhost:27017/")
         self.db = client["velostore_db"]
-
-    #def create_collection(self):
-    #    """Créer une collection avec le schéma défini."""
-    #    if "User" not in self.db.list_collection_names():
-    #        self.db.create_collection('User', validator=self.validator)
-    #        print("Collection 'User' créée avec validation de schéma.")
-    #    else:
-    #        print("La collection 'User' existe déjà.")
 
 velostore_db = VelostoreDatabaseMongo()
--- db/migrations_mongo.py
+++ db/migrations_mongo.py
@@ -1,43 +1,29 @@
-import database_mongo as dbm
-import os
-import sys
-from faker import Faker
+import json
+from database_mongo import VelostoreDatabaseMongo
 
-fake = Faker()
-num_records = 20
-
-sys.path.insert(1, "/".join(os.path.realpath(__file__).split("/")[0:-2]) + "/entities")
-import bike_item_entity as be
-
-#import bike_brand_entity as bbe
-#import order_entity as oe
-#import user_entity as ue
-#import stat_bike_entity as sbe
-#import internal_parameters_entity as ipe
-
-class Migration(dbm.VelostoreDatabaseMongo):
+class Migration(VelostoreDatabaseMongo):
     """Classe pour gérer la migration des données dans la base de données Velostore."""
 
     def __init__(self):
         """Initialise Migration avec les entités nécessaires."""
         super().__init__()
-        self.bike_item_entity = be.BikeItemEntity(self.db)
 
-    def setup(self):
-        """Configure les collections."""
-        self.bike_item_entity.create_collection()
+    def load_json_data(self, file_path):
+        """Charge les données depuis un fichier JSON."""
+        with open(file_path, 'r') as file:
+            return json.load(file)
 
-
-    def add_dynamics_data(self):
-        """Ajoute des données dynamiques aux collections."""
-        self.bike_item_entity.insert_data()
-
+    def insert_data(self, data):
+        """Insère les données dans la base de données."""
+        for collection_name, documents in data.items():
+            collection = self.db[collection_name]
+            collection.insert_many(documents)
 
 def main():
     """Fonction principale pour la classe Migration."""
     initial_setup = Migration()
-    initial_setup.setup()
-    initial_setup.add_dynamics_data()
+    data = initial_setup.load_json_data('data.json')
+    initial_setup.insert_data(data)
 
 if __name__ == "__main__":
     main()
